cd60b38838868513a4305887a3e1e8f59425b42f
Gaspard-Savoureux <samuel.a.lajoie@gmail.com>
2022-04-07 10:56:20 -0400

    XD

--- .#main.py
+++
@@ -1,1 +0,0 @@
-pi@raspberrypi.1343:2
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Samuel Lajoie et Maxime Gazzé
 import os, sys, inspect, time, motor
 import RPi.GPIO as GPIO
+from gpiozero import LED
 from time import sleep
 import I2C_LCD_driver as LCD
 
@@ -16,6 +17,23 @@ iot.ObjConnecteAlive.set_url("wss://alivecode.ca/iotgateway/")
 my_iot = iot.ObjConnecteAlive(object_id = "06ba2eda-60e5-48f7-8d4e-4da9efff35ac")
 
 stationnement = False
+
+led_red_pin = 23
+led_green_pin = 24
+
+led_red = LED(led_red_pin)
+led_green = LED(led_green_pin)
+
+def stationnement_lights_open(isOpen):
+    if isOpen:
+        led_red.off()
+        led_green.on()
+    else:
+        led_red.on()
+        led_green.off()
+
+# set light to red on start
+stationnement_lights_open(False)
 
 @my_iot.on_recv(action_id=10)
 def activateLight(value):
@@ -45,7 +63,9 @@ def main():
                     break
 
                 my_iot.update(projectId, {'/documents/visites/nombre': int(visites[2:])})
+                stationnement_lights_open(True)
                 motor.tourner()
+                stationnement_lights_open(False)
                 sleep(0.5)
 
     except KeyboardInterrupt:
